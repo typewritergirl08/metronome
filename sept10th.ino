f7bfa2514ad4a2576914317e69247a42bf4cb85d
naomi <naomi.tull.wellington@gmail.com>
2025-09-19 09:10:11 +1200

    hmm end of term version
    mr sumner looked over and i need to make different loops if that is possible
    
    anyway wiring: 
    
    Digital pin 0/RX  - mp3 TX
    Digital pin 1/TX  - digital pin RX
    Arduino ground - breadboard ground
    Arduino 5v - breadboard 5V
    MP3 signal out - big speaker +
    MP3 signal out - big speaker -
    MP3 V - breadboard 5v
    MP3 ground - breadboard ground
    Rotary gnd - breadboard ground
    Rotary + - breadboard 5V
    Rotary clk - arduino digital pin 4
    Rotary DT - arduino digital pin 5
    Rotary SW - arduino digital pin 6
     and documented pinouts for lcd, i wrote them in a past commit

--- sept10th.ino
+++ sept10th.ino
@@ -3,16 +3,16 @@
 #include <SoftwareSerial.h> //well duh
 #include <DFRobotDFPlayerMini.h> //mp3
 
-//df mp3 player thing 
+//df mp3 player thing pins
 #define PIN_MP3_RX 2
 #define PIN_MP3_TX 3
 
-//bpm dial
+//bpm dial pins
 #define PIN_CLK 4
 #define PIN_DT  5
 #define PIN_SW  6
 
-//lcd
+//lcd pins
 #define LCD_BACKLIGHT 9
 
 //ok i give up on commenting on everything
@@ -21,9 +21,8 @@ DFRobotDFPlayerMini player;
 
 U8G2_ST7920_128X64_F_SW_SPI u8g2(
   U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* reset=*/ 8
-); // i dont actually use 8 tho it's unwired
+); // i dont actually use 8 tho it's unwired but easier if assigned
 
-int vol = 20; //i dont use this yet though
 int bpm = 60;
 int lastCLK = HIGH;
 bool muted = false;
@@ -38,18 +37,11 @@ void updateLCD() {
     u8g2.drawStr(0, 12, buf);
   }
   
-//  if (muted)
-
-  
-  //  u8g2.drawStr(0, 12, "Muted");
-    //else {
-    //char buf[20];
-    //sprintf(buf, "BPM: %d", bpm);
-    //u8g2.drawStr(0, 12, buf);
-  //}
-
   u8g2.sendBuffer();
 }
+
+unsigned long lastBeat = 0;
+unsigned long interval = 60000 / bpm; //this math is right
 
 void setup() {
   pinMode(LCD_BACKLIGHT, OUTPUT);
@@ -63,10 +55,7 @@ void setup() {
     Serial.println(F("DFPlayer init failed!"));
     while (1);
   }
-  Serial.println(F("DFPlayer initialized"));
-
- // player.volume(vol);
- // Serial.print(F("Volume: ")); Serial.println(vol);
+  Serial.println(F("DFPlayer initialized")); //idk this was example code
 
   pinMode(PIN_CLK, INPUT_PULLUP);
   pinMode(PIN_DT, INPUT_PULLUP);
@@ -75,11 +64,11 @@ void setup() {
   u8g2.begin();
   updateLCD();
 
-  player.play(1);
-  delay(1000);
 }
 
-void loop() {
+void loop() { //is there a way to have a "loop2"? ie something that runs parallel to loop, and also keeps iteratively calling itself
+
+  //put this in its own function
   int clkState = digitalRead(PIN_CLK);
   int dtState  = digitalRead(PIN_DT);
 
@@ -92,37 +81,19 @@ void loop() {
       if (bpm < 60) bpm = 60;
     }
     
-    if (!muted) player.volume(vol);
-    Serial.print(F("BPM: ")); Serial.println(bpm);
-    updateLCD();
+    interval = 60000 / bpm;
   }
-  
-  lastCLK = clkState;
 
-  if (digitalRead(PIN_SW) == LOW) {
-    delay(50);
-    if (digitalRead(PIN_SW) == LOW) {
-      muted = !muted;
-      if (muted) {
-        player.volume(0);
-        Serial.println(F("muted"));
-      } else {
-        player.volume(vol);
-        Serial.println(F("unmuted"));
-      }
+  //------------------------------------------------------------------------------
+ 
       updateLCD();
 
-      while (digitalRead(PIN_SW) == LOW) {
-        delay(10);
-      }
-    }
-  }
-
-unsigned long lastBeat = 0;
-unsigned long interval = 60000 / bpm;
-if (millis() - lastBeat >= interval) {
+  //---------------------------------------------------------------------------
+      
+  //this will need to run independly of the firstloop
+  if (millis() - lastBeat >= interval) {
     lastBeat = millis();
-    player.play(1);
-}
-
+    player.play(1); //i bet the problem is here
+  }
+  
 }
